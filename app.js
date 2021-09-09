f0871edc93a706a20259f340102c68f1f855b290
SanskritiPandey <khushiksp17@gmail.com>
2021-09-09 16:29:35 -0700

    feat: quiz updated

--- app.js
+++ app.js
@@ -1,12 +1,7 @@
 const inputs = document.querySelectorAll(".angleInput");
-// console.log(inputs);
 const triangleBtn = document.querySelector("#btn");
-// console.log(triangleBtn);
 const output1 = document.querySelector("#output");
-// console.log(output1);
 let angle1 ,angle2,angle3;
-
-
 
 function sumOfAngles(angle1, angle2, angle3){
     const sum =angle1+ angle2+ angle3;
@@ -17,19 +12,10 @@ function istriangle(){
 angle1 = parseFloat(inputs[0].value);
 angle2 =  parseFloat(inputs[1].value);
 angle3 =  parseFloat(inputs[2].value);
-// console.log(angle1);
-// console.log(angle2);
-// console.log(angle3);
 let sum = sumOfAngles(angle1,angle2,angle3);
-// console.log(sum);
 if(sum === 180)
 output1.innerText = "YES!! These angles form a triangle.";
  else
 output1.innerText = "SORRY! You choose the wrong angles.";
-
-    
-    
-
 }
-
 triangleBtn.addEventListener("click",istriangle);
