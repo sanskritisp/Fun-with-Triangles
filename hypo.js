0c9aca8826a6a642913b3fbca6272cd5021999d5
SanskritiPandey <khushiksp17@gmail.com>
2021-09-09 17:21:07 -0700

    feat: calculate hypo done

--- hypo.js
+++ hypo.js
@@ -2,11 +2,17 @@ const inputs = document.querySelectorAll(".sides");
 const button = document.querySelector("#btn");
 const output =  document.querySelector("#output");
 
-function calculateHypotenuse(){
-    console.log("hypo");
-
+function calculateHypotenuse()
+{
+  const result = calculateSumOfSquares(Number(inputs[0].value), (inputs[1].value));
+   const finalResult = Math.sqrt(result);
+ output.innerText = "The length of hypotenuse is "+ finalResult;
 }
 
+function calculateSumOfSquares(a,b){
+ let sum = a*a + b*b;
+return sum;
+}
 
 
 button.addEventListener("click",calculateHypotenuse);
