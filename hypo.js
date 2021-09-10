1dcabc29a544e30012123fc17a9830e79bfd427e
SanskritiPandey <khushiksp17@gmail.com>
2021-09-09 17:08:36 -0700

    feat:  started caculate hypo

---
+++ hypo.js
@@ -0,0 +1,14 @@
+const inputs = document.querySelectorAll(".sides");
+const button = document.querySelector("#btn");
+const output =  document.querySelector("#output");
+
+function calculateHypotenuse(){
+    console.log("hypo");
+
+}
+
+
+
+button.addEventListener("click",calculateHypotenuse);
+
+
