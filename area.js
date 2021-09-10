d17cbd108995751b7634db54afca30563f998b0c
SanskritiPandey <khushiksp17@gmail.com>
2021-09-09 18:24:24 -0700

    style: nav bar added

--- area.js
+++ area.js
@@ -6,16 +6,11 @@ function calculateArea(){
     const ans = BaseHeight(Number(inputs[0].value),(inputs[1].value));
     const area = 0.5 * ans;
    output.innerText = "The area of triangle is " + area + "cm square.";
-
-
-
 }
 
 function BaseHeight(a,b){
     let product = a*b;
-    return product;
-
-}
+    return product;}
 
 
 button.addEventListener("click", calculateArea);
