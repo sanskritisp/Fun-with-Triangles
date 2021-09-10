418f63b6788c9e249dafd508d26d9d8efd78c239
SanskritiPandey <khushiksp17@gmail.com>
2021-09-09 18:00:49 -0700

    feat: area of triangle

---
+++ area.js
@@ -0,0 +1,21 @@
+const inputs = document.querySelectorAll(".sides");
+const button = document.querySelector("#btn");
+const output = document.querySelector("#output");
+
+function calculateArea(){
+    const ans = BaseHeight(Number(inputs[0].value),(inputs[1].value));
+    const area = 0.5 * ans;
+   output.innerText = "The area of triangle is " + area + "cm square.";
+
+
+
+}
+
+function BaseHeight(a,b){
+    let product = a*b;
+    return product;
+
+}
+
+
+button.addEventListener("click", calculateArea);
